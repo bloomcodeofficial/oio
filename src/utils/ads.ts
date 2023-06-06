2323f58b4a488994137ea4f02e69e15c4c7994db
niklashans <104555311+niklashans@users.noreply.github.com>
2023-06-06 08:43:46 +0200

    Removed not needed console log

--- src/utils/ads.ts
+++ src/utils/ads.ts
@@ -80,8 +80,6 @@ export const ads = async function () {
       })
       .then((data) => {
         const { jobs } = data;
-
-        console.log(jobs);
         return jobs;
       })
       .catch((error) => {
